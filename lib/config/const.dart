39c9ef90c9f7ba77855b7a4f3c5e6ba7870979d4
baotan2000 <lamphucbaotan@gmail.com>
2023-08-03 16:55:40 +0700

    grid builder, text tittle

--- lib/config/const.dart
+++ lib/config/const.dart
@@ -4,3 +4,8 @@ const dColorMain = Color(0xff0EAF89);
 const dColorIconButtonInActive = Color(0xffdfaff00);
 const dColorIconButtonNotActive = Color(0xffDEDEDE);
 const sizeIconButton = 24.0;
+const styleTittleItem =  TextStyle(
+  color: Color(0xff0eaf89),
+  fontSize: 16,
+  fontWeight: FontWeight.bold
+);
--- lib/pages/home/home_body.dart
+++ lib/pages/home/home_body.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_goodfood/config/const.dart';
+
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child:Text("HomeBody"),
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 15
+      ),
+      itemCount: 5,
+      itemBuilder: (BuildContext context, int index) {
+        return Column(
+          children: [
+            Expanded(
+              flex: 2, 
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage("https://images.unsplash.com/photo-1690380117575-82fb93cf524a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80")
+                  )
+                ),
+              )
+            ),
+            const Expanded(
+              flex: 1, 
+            child: Center(
+              child: Text("Principal Solutions Administrator",
+                style: styleTittleItem,
+                maxLines: 2,
+                textAlign: TextAlign.center,
+              ),
+            )
+            ),
+          ],
+        );
+      },
     );
   }
 }
